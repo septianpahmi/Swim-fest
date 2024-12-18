7ab30979052d45660ebb332aa4ab58e5f90d2a88
septianpahmi <sptnfahmi@gmail.com>
2024-12-18 19:11:39 +0700

    add tailwind css

---
+++ resources/views/Layouts/beranda.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Beranda</title>
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <h1>Beranda</h1>
+</body>
+
+</html>
