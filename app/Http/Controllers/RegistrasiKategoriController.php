f7cb60d77a7e62a6634c414888a3cb552bb5e2ab
Kiki Darmawan <kikiedarmawan@gmail.com>
2024-12-27 08:33:21 +0700

    disabled validasi

--- app/Http/Controllers/RegistrasiKategoriController.php
+++ app/Http/Controllers/RegistrasiKategoriController.php
@@ -30,14 +30,14 @@ class RegistrasiKategoriController extends Controller
     {
         $eventId = Events::where('slug', $slug)->select('id');
         $event = Category_events::where('event_id', $eventId)->first();
-        $userRegistered = Registrations::where('event_id', $eventId)
-            ->where('user_id', Auth::user()->id)->where('type', 'Mandiri')
-            ->exists();
+        // $userRegistered = Registrations::where('event_id', $eventId)
+        //     ->where('user_id', Auth::user()->id)->where('type', 'Mandiri')
+        //     ->exists();
 
-        if ($userRegistered) {
-            return redirect()->back()
-                ->with('error', 'You have already registered for this event.');
-        }
+        // if ($userRegistered) {
+        //     return redirect()->back()
+        //         ->with('error', 'You have already registered for this event.');
+        // }
         $provinsi = Province::orderBy('name', 'asc')->get();
         return view('Resources.form-registrasi-mandiri', compact('event', 'provinsi'));
     }
